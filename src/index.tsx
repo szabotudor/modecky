c4fb1f30dfcc54347818ae0a7661d671415ed8df
szabotudor <szabo.tudor@gmail.com>
2025-08-08 09:58:37 +0300

    Detecting steam games automatically

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,33 @@ import {
   definePlugin,
 } from "@decky/api"
 import {
-  Router,
-  sleep
+  Router
 }
 from "decky-frontend-lib";
 import Logo from "../assets/xelu/Steam Deck/SteamDeck_Power.png";
-import { useEffect, useState } from "react";
-import { AppInfo, SteamInstallFolder } from "@decky/ui/dist/globals/steam-client/InstallFolder";
+import { ReactNode, useEffect, useState } from "react";
+
+class Provider<T> {
+  element: JSX.Element = <div>Empty Provider</div>;
+  setter: React.Dispatch<React.SetStateAction<T>> = (_v: React.SetStateAction<T>) => { throw "invalid setter inside provider"; };
+};
+
+function AppProvider<T extends ReactNode>(default_text: string) {
+  const provider: Provider<T | null> = new Provider<T | null>();
+
+  const component: React.FC = () => {
+    const [app, setApp] = useState<T | null>(null);
+    provider.setter = setApp;
+    provider.element = <div>{app ?? default_text}</div>
+    return provider.element;
+  };
+
+  return {provider, component};
+}
+
+const appid = AppProvider<JSX.Element>("Select a game to continue.");
 
 function Content() {
-  const [appid, setAppid] = useState<string | null>("noapps");
-  
   useEffect(() => {
     const interval = setInterval(() => {
       SteamClient.InstallFolder.RefreshFolders();
@@ -29,19 +45,11 @@ function Content() {
           collected.push(app.nAppID.toString());
         })})
 
-        var allapps = "";
-
         collected.forEach(app => {
-          // if (Router.WindowStore?.GamepadUIMainWindowInstance?.BrowserWindow.document.documentElement.innerHTML.includes(app)) {
-          //   if (appid == null)
-          //     setAppid(app);
-          //   else
-          //     setAppid("noapp");
-          // }
-          allapps += app + '\n';
+          if (Router.WindowStore?.GamepadUIMainWindowInstance?.BrowserWindow.document.documentElement.innerHTML.includes(app)) {
+            appid.provider.setter(<div className={staticClasses.Title}>{app}</div>);
+          }
         })
-
-        setAppid(allapps);
       })
     }, 1000);
 
@@ -51,7 +59,7 @@ function Content() {
   return (
     <PanelSection>
       <PanelSectionRow>
-        <div className={staticClasses.Title}>{appid ?? "??"}</div>
+        <appid.component />
       </PanelSectionRow>
     </PanelSection>
   );
